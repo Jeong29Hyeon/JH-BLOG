e4f5cfc3b22a9ce341bf80615c84632de94c0627
jeong29hyeon <sian0369@naver.com>
2023-06-22 01:01:17 +0900

    블로그 프로젝트 생성
    +spring 2.7.12
    의존성 thymeleaf, web, mabitis, jdbc, lombok

---
+++ src/main/java/com/study/blog/JhBlogApplication.java
@@ -0,0 +1,13 @@
+package com.study.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhBlogApplication.class, args);
+	}
+
+}

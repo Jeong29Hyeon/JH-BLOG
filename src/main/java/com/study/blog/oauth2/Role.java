60e1406f67d7d4c4bee4b1274c7724a37768df35
jeong29hyeon <sian0369@naver.com>
2023-06-27 00:11:52 +0900

    #18 - OAuthAttributes, Role, SocialType 구현
    socialType, userNameAttributeName, Attributes 를 가지고 소셜타입별로 유저 정보가 담긴 UserInfo와, 
    OAuth2 로그인 진행시 키가 되는 필드 값을 가지고 있는 객체이다. 이 객체를 통해 유저 정보를 생성한다.

---
+++ src/main/java/com/study/blog/oauth2/Role.java
@@ -0,0 +1,15 @@
+package com.study.blog.oauth2;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    USER("ROLE_USER","일반 사용자"),
+    GUEST("ROLE_GUEST","손님");
+    private final String key;
+    private final String title;
+}
+

f747637ba07584945eacdab4b7e6075c8575619c
jeong29hyeon <sian0369@naver.com>
2023-06-27 16:37:13 +0900

    #22 - OAuth2 로그인 페이지 제작, 간단 수정
    body, main 의 css를 수정했고, loginPage와 failUrl 을 추가했다.

--- src/main/java/com/study/blog/controller/MainController.java
+++ src/main/java/com/study/blog/controller/MainController.java
@@ -1,28 +1,17 @@
 package com.study.blog.controller;
-
-import com.study.blog.domain.Users;
-import com.study.blog.dto.SessionUser;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
     @GetMapping("/")
-    public String index(Model model, HttpSession session) {
-        SessionUser sessionUser = (SessionUser) session.getAttribute("user");
-        if(sessionUser != null) {
-            model.addAttribute("user",sessionUser);
-        }
-        return "index";
+    public String index() {
+        return "redirect:/post";
     }
 
-
+    @GetMapping("/oauth2-login")
+    public String oauth2Login() {
+        return "login";
+    }
 }

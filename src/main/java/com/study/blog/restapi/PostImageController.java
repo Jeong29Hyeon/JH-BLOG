cae0f7e5099acfd4373b4277771c93dfeeea41b3
jeong29hyeon <sian0369@naver.com>
2023-06-27 12:19:39 +0900

    #22- 포스트 이미지 업로드 구현
    /post-image/upload 에서 MultipartFile 을 받아
    서버에 업로드 시키고 OriginalFileName, SaveFileName을 리턴하는 api 구현

--- src/main/java/com/study/blog/restapi/PostImageController.java
+++ src/main/java/com/study/blog/restapi/PostImageController.java
@@ -2,11 +2,39 @@ package com.study.blog.restapi;
 
 import com.study.blog.service.ImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
-public class ImageController {
+public class PostImageController {
     private final ImageService imageService;
 
+    @PostMapping("/post-image/upload")
+    public ResponseEntity<Map<String,String>> uploadPostImage(
+            @RequestParam("postImage")final MultipartFile file) {
+        Map<String,String> response = new HashMap<>();
+        String saveFileName = null;
+        try {
+            saveFileName = imageService.uploadPostImage(file);
+        } catch (Exception e) {
+            e.printStackTrace();
+            response.put("msg",e.getMessage());
+            return ResponseEntity.badRequest().body(response);
+        }
+        if(saveFileName == null) {
+            response.put("msg","업로드파일이 존재하지 않습니다.");
+            return ResponseEntity.badRequest().body(response);
+        }
+        response.put("saveFileName",saveFileName);
+        String originalFileName = file.getOriginalFilename().substring(0, file.getOriginalFilename().lastIndexOf("."));
+        response.put("originalFileName",originalFileName);
+        return ResponseEntity.ok().body(response);
+    }
 }

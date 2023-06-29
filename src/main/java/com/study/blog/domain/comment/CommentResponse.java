0454d348d37eceab2cceeec9319571ca2147ed51
jeong29hyeon <sian0369@naver.com>
2023-06-30 00:14:23 +0900

    #24 - 댓글 도메인 추가
    요청과, 응답 도메인을 분리해서 추가했다.
    Response의 경우 댓글과 댓글을 쓴 사용자의 ID 를 inner join 해서 매핑해서 가져옴

--- src/main/java/com/study/blog/domain/comment/CommentResponse.java
+++ src/main/java/com/study/blog/domain/comment/CommentResponse.java
@@ -1,18 +1,18 @@
 package com.study.blog.domain.comment;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.study.blog.domain.Users;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class CommentResponse {
     private Long id;
     private Long postId;
     private String content;
-    private Long userId;
+    private Users user;
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
     private Long parentId; //댓글의 그룹을 확인하는 용으로, 댓글 그룹의 부모 PK를 가지게 된다.

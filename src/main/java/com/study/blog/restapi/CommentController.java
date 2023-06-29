3879b261ff202da11ca18d27658790488b7f85c8
jeong29hyeon <sian0369@naver.com>
2023-06-30 00:28:08 +0900

    #24 - 댓글기능 뷰에 붙이기
    댓글기능을 뷰에 붙이고 각 요청을 컨트롤러 uri에 매핑했음

---
+++ src/main/java/com/study/blog/restapi/CommentController.java
@@ -0,0 +1,73 @@
+package com.study.blog.restapi;
+
+import com.study.blog.domain.comment.CommentRequest;
+import com.study.blog.domain.comment.CommentResponse;
+import com.study.blog.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+    private final CommentService commentService;
+
+    @GetMapping("/posts/{postId}/comments") //댓글 조회
+    public List<CommentResponse> findAllComments(
+            @PathVariable final Long postId){
+        return commentService.findComments(postId);
+    }
+
+    @GetMapping("/posts/{postId}/comments/{commentId}")
+    public CommentResponse findOneComment( //댓글 하나 조회
+            @PathVariable final Long postId,
+            @PathVariable final Long commentId) {
+        return commentService.findOne(commentId);
+    }
+
+
+    @PostMapping("/posts/{postId}/comments") //신규 댓글 저장
+    public Map<String,Object> saveComment(
+            @PathVariable final Long postId,
+            @RequestBody final CommentRequest commentRequest) {
+        log.info("CommentController.saveComment() => 파라미터 : "+ commentRequest.toString());
+        Map<String,Object> response = new HashMap<>();
+        commentService.save(commentRequest);
+        response.put("msg","댓글이 저장되었습니다.");
+        return response;
+    }
+
+    @PatchMapping("/posts/{postId}/comments/{id}")
+    public Map<String,Object> updateComment( //댓글 수정
+            @PathVariable final Long postId,
+            @PathVariable final Long id,
+            @RequestBody final CommentRequest commentRequest) {
+        commentService.update(commentRequest);
+        Map<String,Object> response = new HashMap<>();
+        response.put("msg","댓글이 수정되었습니다.");
+        return response;
+    }
+
+    //댓글 삭제
+    @DeleteMapping("/posts/{postId}/comments/{id}")
+    public Map<String,Object> deleteComment(@PathVariable final Long postId,
+                                            @PathVariable final Long id) {
+        Map<String,Object> response = new HashMap<>();
+        try {
+            commentService.delete(id);
+        } catch (Exception e) {
+            response.put("msg",e.getMessage());
+            return response;
+        }
+        response.put("msg","댓글이 삭제되었습니다.");
+        return response;
+    }
+
+
+
+}
--- src/test/java/com/study/blog/service/CommentServiceTest.java
+++ src/test/java/com/study/blog/service/CommentServiceTest.java
@@ -19,11 +19,11 @@ class CommentServiceTest {
     @DisplayName("댓글을 저장한다.")
     public void saveComment() throws Exception {
         //given
-        CommentRequest commentRequest = CommentRequest.builder().postId(13L).content("댓글1")
+        CommentRequest commentRequest = CommentRequest.builder().postId(13L).content("댓글3")
                                         .userId(3L).parentId(0L).depth(1).build();
         //when
         Long savedId = commentService.save(commentRequest);
-        List<CommentResponse> comments = commentService.findComments();
+        List<CommentResponse> comments = commentService.findComments(13L);
 
         //then
         comments.stream().filter((comment)-> comment.getId().equals(savedId)).findFirst()
@@ -34,8 +34,8 @@ class CommentServiceTest {
     @DisplayName("대댓글을 저장한다.")
     public void saveReply() throws Exception {
         //given
-        CommentRequest commentRequest = CommentRequest.builder().postId(13L).content("댓글1의 대댓글")
-                .userId(3L).parentId(2L).depth(2).build();
+        CommentRequest commentRequest = CommentRequest.builder().postId(13L).content("댓글3의 1")
+                .userId(3L).parentId(6L).depth(2).build();
         //when
         Long savedId = commentService.save(commentRequest);
         List<CommentResponse> comments = commentService.findReplies(2L);
@@ -44,4 +44,13 @@ class CommentServiceTest {
             System.out.println(comment.getContent());
         }
      }
+
+     @Test
+     @DisplayName("모든 댓글을 조회하면 댓글의 정보와 회원의 정보도 나와야한다.")
+     public void findComments() throws Exception {
+         List<CommentResponse> comments = commentService.findComments(13L);
+         for (CommentResponse comment : comments) {
+             System.out.println("comment.toString() = " + comment.toString());
+         }
+      }
 }

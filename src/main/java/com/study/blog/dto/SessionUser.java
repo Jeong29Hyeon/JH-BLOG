62c4abeeaf998ec1d84f3fcbf63a1801bd7f613f
jeong29hyeon <sian0369@naver.com>
2023-06-30 00:27:31 +0900

    #24 - User, SessionUser 수정
    댓글에서 저장,수정할 때 userId가 필요하기 때문에
    필드를 추가했다.

--- src/main/java/com/study/blog/dto/SessionUser.java
+++ src/main/java/com/study/blog/dto/SessionUser.java
@@ -6,10 +6,12 @@ import lombok.Getter;
 
 @Getter
 public class SessionUser {
+    private final Long id; //db상의 유저 pk
     private final String nickname;
     private final String image;
 
     public SessionUser(Users user) {
+        this.id = user.getId();
         this.nickname = user.getNickname();
         this.image = user.getImageUrl();
     }

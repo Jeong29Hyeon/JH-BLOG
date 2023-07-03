6bce46db6c67b083e2a331b5e81b454b7cd3358d
jeong29hyeon <sian0369@naver.com>
2023-07-03 22:03:22 +0900

    #33 - post findAll() 동적쿼리문 변경
    키워드와 해시태그가 없을 시 delete_yn=0을 추가해 논리적으로 삭제된 게시글 안보이게 변경

--- src/main/java/com/study/blog/controller/PostController.java
+++ src/main/java/com/study/blog/controller/PostController.java
@@ -29,48 +29,48 @@ public class PostController {
 
     @GetMapping()
     public String openBlog(@ModelAttribute("searchDto") final SearchDto searchDto,
-                            Model model) {
+                           Model model) {
         System.out.println("searchDto.toString() = " + searchDto.toString());
-        model.addAttribute("posts",postService.findAllPost(searchDto));
-        model.addAttribute("hashtags",hashtagService.findAll());
+        model.addAttribute("posts", postService.findAllPost(searchDto));
+        model.addAttribute("hashtags", hashtagService.findAll());
         return "post/main";
     }
 
     @GetMapping("/write")
-    public String openWriteForm(@RequestParam(value="id",required = false)final Long id,
+    public String openWriteForm(@RequestParam(value = "id", required = false) final Long id,
                                 Model model) {
-        if(id != null) {
+        if (id != null) {
             PostResponse findPost = postService.findPostById(id);
             List<String> hashtags = postTagMapper.findNameByPostId(id);
-            model.addAttribute("post",findPost);
-            model.addAttribute("hashtags",hashtags);
+            model.addAttribute("post", findPost);
+            model.addAttribute("hashtags", hashtags);
         }
         return "post/write";
     }
 
-    @PostMapping(value = "/save", produces = "application/json;charset=UTF-8")
+    @PostMapping(value = "/save")
     @ResponseBody
     public ResponseEntity<Map<String, String>> savePost(
-            @RequestParam(value = "image", required = false)MultipartFile file,
+            @RequestParam(value = "image", required = false) MultipartFile file,
             final PostRequest postRequest,
-            @RequestParam(value = "hashtags",required = false) List<String> tagNames){
-        Map<String,String> response = new HashMap<>();
+            @RequestParam(value = "hashtags", required = false) List<String> tagNames) {
+        Map<String, String> response = new HashMap<>();
         String saveFileName = null;
         try {
             saveFileName = s3Uploader.uploadImage(file);
             s3Uploader.deleteTempPostImages(postRequest.getContent());
         } catch (Exception e) {
-            response.put("errorMsg",e.getMessage());
+            response.put("errorMsg", e.getMessage());
             return ResponseEntity.badRequest().body(response);
         }
         // 이미지 파일이 없으면 default.gif 로 썸네일 잡음
-        if(saveFileName == null) {
+        if (saveFileName == null) {
             postRequest.setThumbnail("images/default.gif");
-        }else {
+        } else {
             postRequest.setThumbnail(saveFileName);
         }
-        Long id = postService.savePost(postRequest,tagNames);
-        response.put("msg","포스트가 저장되었습니다.");
+        Long id = postService.savePost(postRequest, tagNames);
+        response.put("msg", "포스트가 저장되었습니다.");
         response.put("id", String.valueOf(id));
         return ResponseEntity.ok(response);
     }
@@ -79,43 +79,43 @@ public class PostController {
     public String viewPost(@RequestParam("id") final Long id,
                            Model model) {
         PostResponse findPost = postService.findPostById(id);
-        model.addAttribute("post",findPost);
-        model.addAttribute("hashtags",postTagMapper.findNameByPostId(id));
+        model.addAttribute("post", findPost);
+        model.addAttribute("hashtags", postTagMapper.findNameByPostId(id));
         return "post/view";
     }
 
     @PostMapping("/delete")
-    public String deletePost(@RequestParam("id")final Long id) {
+    public String deletePost(@RequestParam("id") final Long id) {
         postService.deletePost(id);
         return "redirect:/post";
     }
 
     @PostMapping("/update")
     @ResponseBody
-    public ResponseEntity<Map<String,String>> updatePost(
-            @RequestParam(value = "image", required = false)MultipartFile file,
+    public ResponseEntity<Map<String, String>> updatePost(
+            @RequestParam(value = "image", required = false) MultipartFile file,
             final PostRequest postRequest,
-            @RequestParam(value = "hashtags",required = false) List<String> tagNames) {
-        Map<String,String> response = new HashMap<>();
+            @RequestParam(value = "hashtags", required = false) List<String> tagNames) {
+        Map<String, String> response = new HashMap<>();
         String saveFileName = null;
         try {
             // 글 수정에서 file이 null이면 썸네일을 수정하지 않았다는 의미다.
             saveFileName = s3Uploader.uploadImage(file); //이미지를 서버 upload 폴더에 저장한다.
             s3Uploader.deleteTempPostImages(postRequest.getContent());
         } catch (Exception e) {
-            response.put("errorMsg",e.getMessage());
+            response.put("errorMsg", e.getMessage());
             return ResponseEntity.badRequest().body(response);
         }
 
         String originThumbnail = postService.findPostById(postRequest.getId()).getThumbnail();
-        if(saveFileName != null) { //새로운 이미지가 들어오면 기존 이미지는 폴더에서 삭제하고 새로 추가
+        if (saveFileName != null) { //새로운 이미지가 들어오면 기존 이미지는 폴더에서 삭제하고 새로 추가
             s3Uploader.deleteImage(originThumbnail);
             postRequest.setThumbnail(saveFileName);
         } else { // 이미지 파일이 없으면 썸네일 그대로 냅둬야함
             postRequest.setThumbnail(originThumbnail);
         }
         Long id = postService.updatePost(postRequest, tagNames);
-        response.put("msg","포스트가 수정되었습니다.");
+        response.put("msg", "포스트가 수정되었습니다.");
         response.put("id", String.valueOf(id));
         return ResponseEntity.ok(response);
     }
